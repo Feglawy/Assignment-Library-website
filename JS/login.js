435657a910d9def19d725511f18aaa5c6abf12f3
Feglawy <a.m9518520963@outlook.com>
2024-03-16 05:53:55 +0200

    fixied some issues

--- JS/login.js
+++ JS/login.js
@@ -1,11 +1,14 @@
 function ShowPassword() {
   var passwordBox = document.getElementById("inp-pass");
+  var confirmPasswordBox = document.getElementById("confirm-inp-pass");
   var iconBox = document.getElementById("visibility");
   if (passwordBox.type === "password") {
     passwordBox.type = "text";
+    confirmPasswordBox.type = "text";
     iconBox.innerHTML = "visibility_off";
   } else {
     passwordBox.type = "password";
+    confirmPasswordBox.type = "password";
     iconBox.innerHTML = "visibility";
   }
 }
