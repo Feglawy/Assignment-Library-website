afed81fc550f390e3e5e67d8d00e0d1c7ae17093
Feglawy <a.m9518520963@outlook.com>
2024-04-06 05:26:19 +0200

    changed show pass icon

--- JS/login.js
+++ JS/login.js
@@ -2,13 +2,22 @@ function ShowPassword() {
   var passwordBox = document.getElementById("inp-pass");
   var confirmPasswordBox = document.getElementById("confirm-inp-pass");
   var iconBox = document.getElementById("visibility");
+
   if (passwordBox.type === "password") {
+    // change icon
+    iconBox.classList.remove("fa-eye");
+    iconBox.classList.add("fa-eye-slash");
+
+    // show password
     passwordBox.type = "text";
-    iconBox.innerHTML = "visibility_off";
     confirmPasswordBox.type = "text";
   } else {
+    // change icon
+    iconBox.classList.remove("fa-eye-slash");
+    iconBox.classList.add("fa-eye");
+
+    // show password
     passwordBox.type = "password";
-    iconBox.innerHTML = "visibility";
     confirmPasswordBox.type = "password";
   }
 }
