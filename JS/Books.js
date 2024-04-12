db1d33db46f50fb44dea2b4ff0ed2a3dc0e3e6ef
Feglawy <a.m9518520963@outlook.com>
2024-04-12 11:26:06 +0200

    added the popup window to confirm deletion

--- JS/Books.js
+++ JS/Books.js
@@ -1,36 +1,67 @@
-// // checls if the device is a mobile device or not
-// function isMobileDevice() {
-//   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|SamsungBrowser/i.test(
-//     navigator.userAgent
-//   );
-// }
+function removeBtnParent(button) {
+  // Get the parent element
+  const parentDiv = button.parentNode;
+  parentDiv.remove();
+}
 
-// // disable the hover effect of the books if the device is a mobile device
-// var bookDivs = document.querySelectorAll(".book");
-// if (isMobileDevice()) {
-//   bookDivs.forEach(function (bookDiv) {
-//     bookDiv.classList.add("no-hover");
-//   });
-// }
+function showPopup() {
+  let popup = document.getElementById("popup");
+  popup.style.display = "block";
+  setTimeout(() => {
+    popup.classList.remove("hidden");
+  }, 100);
+}
+function hidePopup() {
+  let popup = document.getElementById("popup");
+  popup.classList.add("hidden");
+  setTimeout(() => {
+    popup.style.display = "none";
+  }, 200);
+}
+
+function cancelDelete() {
+  hidePopup();
+}
 
 // wait until the book is loaded
 document.addEventListener("BooksAdded", function () {
   // Get all anchor elements with the specified class name
-  let BorrowedBtns = document.querySelectorAll(".remove-borrowed-book");
+  let RemoveBorrowedBtns = document.querySelectorAll(".remove-borrowed-book");
 
-  // Loop through each anchor element
-  BorrowedBtns.forEach(function (button) {
+  let popupElements = `<div id="overlay">
+        <div id="confirmationPopup">
+          <h1>Are you sure you want to delete the book?</h1>
+          <div class="inputs">
+            <button id="ConfirmDeletion">Yes</button>
+            <button id="cancelDeletion">No</button>
+          </div>
+        </div>
+      </div>`;
+
+  let popupDiv = document.getElementById("popup");
+  popupDiv.innerHTML = popupElements;
+
+  RemoveBorrowedBtns.forEach(function (button) {
     button.addEventListener("click", function () {
-      // Get the parent element
-      const parentDiv = button.parentNode;
-      parentDiv.remove();
+      let btn = button;
+      let confirm = document.getElementById("ConfirmDeletion");
+      let cancel = document.getElementById("cancelDeletion");
+
+      showPopup();
+      confirm.addEventListener("click", function () {
+        removeBtnParent(btn);
+        hidePopup();
+      });
+      cancel.addEventListener("click", cancelDelete);
     });
   });
+
+  // show book's details
   var previewButtons = document.querySelectorAll(".previewButton");
   previewButtons.forEach(function (button) {
-    // Add click event listener to the button
+    // click event listener to the button
     button.addEventListener("click", function () {
-      // Get the book ID from the custom 'data-book-id' attribute
+      // Get the book ID from the custom 'book-id' attribute
       var bookId = button.getAttribute("book-id");
 
       // Redirect to the preview page with the corresponding book ID
