3ea9405b42cac1f96293f6b45e553e0422994f25
Cekymeky <islamyasser840@gmail.com>
2024-04-15 14:30:51 +0200

    added the overlay for addNewbtn (not finished)

--- JS/UpdateBooks.js
+++ JS/UpdateBooks.js
@@ -19,23 +19,67 @@ document.addEventListener("DOMContentLoaded", function () {
   document.dispatchEvent(BooksAdded);
 });
 
-let addNewBtn = document.getElementById("add-new");
+// let addNewBtn = document.getElementById("");
 
-addNewBtn.addEventListener("click", addBook);
-//add new button
-function addBook(value, key) {
-  let bk = `
-<div class="book">
-    <img src="/Images/covers/Blank.png">
-    <h5 class="name">Blank </h5>
-    <button>Edit</button>
-    <button class="Delete">Delete</button>
-</div>
-`;
-  // add the book to the page
-  let booksSection = document.getElementById("Update-books-section");
-  booksSection.innerHTML += bk;
-  // event listner to ensure that the books added
-  const BooksAdded = new Event("BooksAdded");
-  document.dispatchEvent(BooksAdded);
+// addNewBtn.addEventListener("click", addBook);
+// //add new button
+// function addBook(value, key) {
+//   let bk = `
+// <div class="book">
+//     <img src="/Images/covers/Blank.png">
+//     <h5 class="name">Blank </h5>
+//     <button>Edit</button>
+//     <button class="Delete">Delete</button>
+// </div>
+// `;
+//   // add the book to the page
+//   let booksSection = document.getElementById("Update-books-section");
+//   booksSection.innerHTML += bk;
+//   // event listner to ensure that the books added
+//   const BooksAdded = new Event("BooksAdded");                                              WILL DO IT LATER
+//   document.dispatchEvent(BooksAdded);
+// }
+
+////////////////////// POPUP /////////////////////
+
+const openModalBtn = document.querySelectorAll('[data-modal-target]');
+const closeModalBtn = document.querySelectorAll('[data-close-btn]');
+const overlayModal = document.getElementById('overlay-modal');
+
+openModalBtn.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log("Button clicked");
+    const modalId = button.getAttribute('data-modal-target');
+    console.log("Modal ID:", modalId);
+    const modal = document.querySelector(modalId);
+    console.log("Modal:", modal);
+    openModal(modal);
+  });
+});
+
+
+overlayModal.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach(modal => {
+    closeModal(modal);
+  });
+});
+
+closeModalBtn.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if(modal == null) {return}
+  modal.classList.add('active');
+  overlayModal.classList.add('active');
+}
+
+function closeModal(modal) {
+  if(modal == null) {return}
+  modal.classList.remove('active');
+  overlayModal.classList.remove('active');
 }
