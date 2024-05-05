6d93d6711a1297cae9f871d9b81e46321e06d220
Feglawy <a.m9518520963@outlook.com>
2024-05-05 06:58:42 +0300

    ...

--- accounts/forms.py
+++ accounts/forms.py
@@ -24,14 +24,6 @@ class LoginForm(AuthenticationForm):
         fields = {'username', 'password'}
 
 class SignupForm(UserCreationForm):
-    """
-    widgets = {
-                'first_name': forms.TextInput(attrs={'class': 'input', 'id': 'first-name-inp', 'placeholder': 'First name', 'required': True}),
-                'last_name': forms.TextInput(attrs={'class': 'input', 'id': 'last-name-inp', 'placeholder': 'Last name'}),
-                'username': forms.TextInput(attrs={'class': 'input', 'id': 'inp-username', 'placeholder': 'Enter your username', 'required': True}),
-                'email': forms.EmailInput(attrs={'class': 'input', 'id': 'inp-email', 'placeholder': 'Enter your email'}),
-            }
-    """
     def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
 
@@ -85,12 +77,6 @@ class SignupForm(UserCreationForm):
     ]
     user_type = forms.ChoiceField(choices=USER_TYPES)
 
-    # password1 = forms.CharField(
-    #     widget=forms.PasswordInput(attrs={'class': 'input', 'id': 'inp-pass', 'placeholder': 'Enter your password'}),
-    # )
-    # password2 = forms.CharField(
-    #     widget=forms.PasswordInput(attrs={'class': 'input', 'id': 'confirm-inp-pass', 'placeholder': 'Confirm your password'}),
-    # )
     class Meta:
         model = CustomUser
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'user_type']
