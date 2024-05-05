690e6d2b9a2594c27d5969681c049aecea730e30
Feglawy <a.m9518520963@outlook.com>
2024-05-05 17:59:32 +0300

    added block title to the base to modefiy the page's titles

--- django_library_website/settings.py
+++ django_library_website/settings.py
@@ -16,6 +16,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'library/static'),
+    os.path.join(BASE_DIR, 'accounts/static'),
+]
+
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
