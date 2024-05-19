885fbdfe6a96589b464e0c746f3065df049cb93e
Feglawy <a.m9518520963@outlook.com>
2024-05-19 12:35:04 +0300

    made the delete popup and it is done

--- static/JS/UpdateBooks.js
+++ static/JS/UpdateBooks.js
@@ -1,184 +1,71 @@
-// import { bookMap } from "./AvailableBooks.js";
+document.addEventListener("DOMContentLoaded", () => {
+  let book_name = document.getElementById("Book_name");
+  const deleteBtns = document.querySelectorAll(".Delete");
+  const popupOverlay = document.getElementById("popup_overlay");
+  const confirmBtn = document.getElementById("confirm_button");
+  const cancelBtn = document.getElementById("cancel_button");
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   let booksSection = document.getElementById("Update-books-section");
-//   bookMap.forEach((value, key) => {
-//     let Book = `
-// <div class="book">
-//     <img src=${value.cover} alt="${key}" />
-//     <h5 class="name">${value.tittle}</h5>
-//     <button class="Edit">Edit</button>
-//     <button class="Delete">Delete</button>
-// </div>
-// `;
-//     booksSection.innerHTML += Book;
-//   });
+  let bookId;
 
-//   // Attach event listener for edit buttons
-//   editFunction();
-
-//   // Event listener to ensure that the books added
-//   const BooksAdded = new Event("BooksAdded");
-//   document.dispatchEvent(BooksAdded);
-// });
-
-// POPUP
-const openModalBtn = document.querySelectorAll("[data-modal-target]");
-const closeModalBtn = document.querySelectorAll("[data-close-btn]");
-const overlayModal = document.getElementById("overlay-modal");
-
-openModalBtn.forEach((button) => {
-  button.addEventListener("click", () => {
-    const modalId = button.getAttribute("data-modal-target");
-    const modal = document.querySelector(modalId);
-    openModal(modal);
-  });
-});
-
-overlayModal.addEventListener("click", () => {
-  const modals = document.querySelectorAll(".modal.active");
-  modals.forEach((modal) => {
-    closeModal(modal);
-  });
-});
-
-closeModalBtn.forEach((button) => {
-  button.addEventListener("click", () => {
-    const modal = button.closest(".modal");
-    closeModal(modal);
-  });
-});
-
-function openModal(modal) {
-  if (modal == null) {
-    return;
-  }
-  document.body.style.overflow = "hidden"; // Disable page scrolling
-  modal.classList.add("active");
-  overlayModal.classList.add("active");
-}
-
-function closeModal(modal) {
-  if (modal == null) {
-    return;
-  }
-  document.body.style.overflow = "auto"; // Enable page scrolling
-  modal.classList.remove("active");
-  overlayModal.classList.remove("active");
-}
-
-document.querySelector("#uploadInput").addEventListener("change", function () {
-  const file = this.files[0];
-  const reader = new FileReader();
-
-  reader.onload = function (e) {
-    const imgElement = document.querySelector("#uploadedImage"); //To display the book's cover
-    imgElement.src = e.target.result;
-  };
-
-  if (file) {
-    reader.readAsDataURL(file);
-  }
-});
-
-//////////////////////////////////////    Submit button functionality                          /////////////////////////////////////////////////////
-
-document.getElementById("Submit-book").addEventListener("click", addBook);
-
-function addBook() {
-  const title = document.querySelector("#modal input[type='title']").value;
-  const author = document.querySelector("#modal input[type='author']").value;
-  const description = document.querySelector(
-    "#modal input[type='description']"
-  ).value;
-  const type = document.querySelector("#modal input[type='type']").value;
-  const cover = document.querySelector("#uploadedImage").src;
-
-  let bookHTML = `
-    <div class="book">
-      <img src="${cover}" alt="${title}" />
-      <h5 class="name">${title}</h5>
-      <button class="Edit">Edit</button>
-      <button class="Delete">Delete</button>
-    </div>
-  `;
-
-  let booksSection = document.getElementById("Update-books-section");
-  booksSection.innerHTML += bookHTML;
-
-  // event listner to ensure that the books added
-  const BooksAdded = new Event("BooksAdded");
-  document.dispatchEvent(BooksAdded);
-
-  const modal = document.getElementById("modal");
-  closeModal(modal);
-
-  // to be able to use the edit on the new added books
-  editFunction();
-}
-
-// Function to attach event listener for edit buttons
-function editFunction() {
-  const editButtons = document.querySelectorAll(".Edit");
-  editButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const bookDiv = button.closest(".book");
-      const title = bookDiv.querySelector(".name").textContent;
-      const cover = bookDiv.querySelector("img").src;
-
-      // Add the "editing" class to the book element
-      bookDiv.classList.add("editing");
-
-      // putting the existing data in the popup
-      document.getElementById("edit-uploadedImage").src = cover;
-      document.getElementById("edit-book-title").value = title;
-
-      // Open the edit popup
-      const modal = document.getElementById("edit-modal");
-      openModal(modal);
+  deleteBtns.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      bookId = event.target.getAttribute("book_id");
+      book_name.innerHTML = event.target.getAttribute("book_name");
+      popupOverlay.classList.add("visible");
     });
   });
-}
 
-document.getElementById("save-edit").addEventListener("click", editSave);
+  const closePopup = () => {
+    popupOverlay.classList.remove("visible");
+  };
 
-document
-  .querySelector("#edit-uploadInput")
-  .addEventListener("change", function () {
-    const file = this.files[0];
-    const reader = new FileReader();
+  confirmBtn.addEventListener("click", async () => {
+    try {
+      const response = await fetch(`/api/book/delete/${bookId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": getCookie("csrftoken"),
+        },
+      });
+      if (response.ok) {
+        const successMessage = "Book deleted!";
+        toastr.success(successMessage, "Success", {
+          closeButton: true,
+          progressBar: true,
+        });
+      } else {
+        const failedMessage = "There an error occured!";
+        toastr.error(failedMessage, "Error");
+      }
+    } catch (error) {
+      alert("Error: " + error.message);
+    }
+    closePopup();
+  });
 
-    reader.onload = function (e) {
-      console.log("File read successfully:", e.target.result);
-      const imgElement = document.querySelector("#edit-uploadedImage");
-      console.log("Image element:", imgElement);
-      imgElement.src = e.target.result;
-    };
+  cancelBtn.addEventListener("click", () => {
+    closePopup();
+  });
 
-    if (file) {
-      reader.readAsDataURL(file);
+  popupOverlay.addEventListener("click", (e) => {
+    if (e.target === popupOverlay) {
+      closePopup();
     }
   });
-function editSave() {
-  const newCover = document.getElementById("edit-uploadedImage").src;
-  const newTitle = document.getElementById("edit-book-title").value;
-  console.log("test");
+});
 
-  const editedBook = document.querySelector(".book.editing");
-  console.log("editedBook:", editedBook);
-  const coverElement = editedBook.querySelector("img");
-  console.log("edited img");
-
-  const titleElement = editedBook.querySelector(".name");
-  console.log("edited naame");
-
-  coverElement.src = newCover;
-  titleElement.textContent = newTitle;
-
-  editedBook.classList.remove("editing");
-
-  // close popup
-  const modal = document.getElementById("edit-modal");
-  closeModal(modal);
-  console.log("Changes saved successfully!");
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
 }
